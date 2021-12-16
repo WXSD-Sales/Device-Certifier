3800c36cdf509c0fc45d62e2b5d5b2f567d4e9a7
Arash Koushkebaghi <arash.koush@gmail.com>
2021-12-16 15:01:30 -0800

    Authetication is complete now

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const CLIENT_ID = 'Ce5d2e0f83ab03c3749c92702c1a6517820953f6236198e81806749ddb0d07bec';
+export const CLIENT_SECRET = 'b8fa12015cbf95ee5319bef9c9c4821cad50a983dff52d1595bebd22d2f2ad82';
+export const AUTHORIZATION_URL = 'https://webexapis.com/v1/authorize?client_id=Ce5d2e0f83ab03c3749c92702c1a6517820953f6236198e81806749ddb0d07bec&response_type=code&redirect_uri=https%3A%2F%2Fdevice-certifier.ngrok.io&scope=spark%3Aall%20spark%3Akms&state=state';
+export const GRANT_TYPE = "authorization_code";
+export const REFRESH_GRANT_TYPE = "refresh_token";
+export const REDIRECT_URL = "https://device-certifier.ngrok.io";
+export const WEBEX_AUTH_URL = "https://webexapis.com/v1/access_token";
